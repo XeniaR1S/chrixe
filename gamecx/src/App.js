1b6212d084dd738b8e9bc71dc5e1ab2db2b1485a
XeniaR1S <lecomtep.x@gmail.com>
2020-01-28 15:27:56 +0100

    Add picture on components

--- gamecx/src/App.js
+++ gamecx/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Begin from './compenents/Begin';
+import StairsDown from './compenents/StairsDown';
 
 function App() {
   return (
     <div className="App">
-
+      <Begin/>
+      <StairsDown/>
     </div>
   );
 }
