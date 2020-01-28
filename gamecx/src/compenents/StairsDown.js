d890cdf1ee30657839c42238a13d2cfe637c4f95
XeniaR1S <lecomtep.x@gmail.com>
2020-01-28 11:26:09 +0100

    Create differents components to work easyer later

---
+++ gamecx/src/compenents/StairsDown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class StairsDown extends React.Component {
+  render (){
+  return (
+    <div className="stairs-down">
+      Stairs down
+    </div>
+  );
+}
+}
+
+export default StairsDown;
---
+++ gamecx/src/compenents/StayEat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class StayEat extends React.Component {
+  render (){
+  return (
+    <div className="stay-eat">
+      Stay Eat
+    </div>
+  );
+}
+}
+
+export default StayEat;
