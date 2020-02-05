b5d9a75bd46be5e76dca9b400f3397f905374ffc
Christopher Poncelet <christopher.poncelet51@gmail.com>
2020-02-05 16:20:22 +0100

    apply card in others components

--- gamecx/src/compenents/Begin.js
+++ gamecx/src/compenents/Begin.js
@@ -13,12 +13,14 @@ class Begin extends React.Component {
     return (
       <div className="begin">
         <div className='card'>
+          
           <img className='imgCard' src={cabane} alt='cabane'/>
+       
           <p className="textCard">Tu ouvres durement les yeux. Tu as froid. Tu es allongé sur le sol humide. Aucun souvenirs, qui es-tu ? Comment t'appelles-tu ? Comment es-tu arrivé là ? Une seconde.. Un prénom résonne dans ta tête.. Jojo .. Ca doit être le tiens . Tu ne vois que la cime des arbres qui t'entourent. Tu te relèves, la forêt autour de toi est grise et très peu accueillante, ça doit être l'hiver. Devant toi, une vieille cabane faite de bois qui, visiblement, ne va pas tarder à s'écrouler. Il n'y a rien d'autre aux alentours. Tu avances doucement, et entre dans la cabane. Deux portes s'offrent à toi. L'une est d'un blanc luisant et paraît très moderne, l'autre, quant à elle, est d'un bois visiblement très ancien et toutes ses parties en métal sont rouillées. Laquelle choisis-tu ?</p>
         </div>
         <div className="choiceDoor">
-          <Link to="/whitedoor">Porte blanche</Link>
-          <Link to="/olddoor">Porte en bois</Link>
+          <Link className="linkTo" to="/whitedoor">Porte blanche</Link>
+          <Link className="linkTo" to="/olddoor">Porte en bois</Link>
         </div>
       </div>
     );
--- gamecx/src/compenents/DoorWhite.js
+++ gamecx/src/compenents/DoorWhite.js
@@ -7,15 +7,20 @@ import '../App.css';
 class DoorWhite extends React.Component {
   render (){
     return (
-      <div className="door-white">
-        <figure className='figure-crypte'>
-          <img className='img-crypte' src={crypte} alt='crypte'/>
-        </figure>
-        <p>
+      <div className="door-white begin">
+        <div className='card'>
+          <img className='img-crypte imgCard' src={crypte} alt='crypte'/>
+        
+        <p className="textCard">
           Tu atteris dans une petite crypte. Il fait sombre. La seul lumière vient d'une torche accrochée sur un mur à ta droite. En face de toi, deux escaliers en colimaçon taillés dans la pierre. L'un monte l'autre descend, lequel vas-tu emprunter ? 
                   </p>
-        <Link to="/stairsup">Monter</Link>
-        <Link to="/stairsdown">Descendre</Link>
+                  </div>
+
+        <div className="choiceDoor">
+
+        <Link className="linkTo" to="/stairsup">Monter</Link>
+        <Link className="linkTo" to="/stairsdown">Descendre</Link>
+        </div>
       </div>
     );
   }
